cafd083f67e5bb7968fc3b6791156efcb0d8b953
Edilson Borges <edilsonfb@gmail.com>
2017-04-16 19:11:20 -0300

    JS Stalker Exercise

---
+++ js-basics/ex.3-stalker-exercise/script.js
@@ -0,0 +1,5 @@
+var userFirstName = prompt('First name?');
+var userLastName = prompt('Last name?');
+var userAge = prompt('Age?');
+console.log('Your full name is '+userFirstName+' '+userLastName);
+console.log('You are '+userAge+' years old');
