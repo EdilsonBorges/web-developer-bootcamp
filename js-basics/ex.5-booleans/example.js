9385f636e9cfa0217a9f7c39206322ad4e5d2053
Edilson Borges <edilsonfb@gmail.com>
2017-04-17 06:54:11 -0300

    Truthy and Falsy Values

--- js-basics/ex.5-booleans/example.js
+++ js-basics/ex.5-booleans/example.js
@@ -10,3 +10,15 @@ y === "b" || x >= 10
 var x = 3;
 var y = 8;
 !(x == "3" || x === y) && !(y != 8 && x <= y)
+
+//Truthy and Falsy Values
+//Values that aren't actually true or false,
+//are still inherently "truthy" or "falsey"
+//when evaluated in a boolean context
+
+!!"Hi There" //true
+!!""         //false
+!!null       //false
+!!0          //false
+!!-1         //true
+!!NaN        //false
