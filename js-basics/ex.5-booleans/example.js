5e8ec46ee954ad337e5eb9226e07c2d74d0b80ae
Edilson Borges <edilsonfb@gmail.com>
2017-04-17 07:20:31 -0300

    While loops with str.length and str[count]

--- js-basics/ex.5-booleans/example.js
+++ js-basics/ex.5-booleans/example.js
@@ -55,3 +55,10 @@ while(count < 6) {
     console.log("count is: "+count);
     count++;
 }
+
+var str = "hello";
+var count = 0;
+while(count < str.length){
+    console.log(str[count]);
+    count++;
+}
