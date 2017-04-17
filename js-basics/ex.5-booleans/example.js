a76837f2776718e61a32a6aa9cc98c97abc16030
Edilson Borges <edilsonfb@gmail.com>
2017-04-17 06:47:43 -0300

    Basic logic expressions

--- js-basics/ex.5-booleans/example.js
+++ js-basics/ex.5-booleans/example.js
@@ -2,3 +2,11 @@ true == "1"         //true
 0 == false          //true
 null == undefined   //true
 NaN == NaN          //false (not a number)
+
+var x = 10;
+var y = "a";
+y === "b" || x >= 10
+
+var x = 3;
+var y = 8;
+!(x == "3" || x === y) && !(y != 8 && x <= y)
