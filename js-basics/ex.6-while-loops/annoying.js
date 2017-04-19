e8220799410a4b12fc35be08a9f52e95b5e500dd
Edilson Borges <edilsonfb@gmail.com>
2017-04-19 07:21:12 -0300

    Annoying asks correction

--- js-basics/ex.6-while-loops/annoying.js
+++ js-basics/ex.6-while-loops/annoying.js
@@ -1,4 +1,7 @@
 var answer = prompt("Are we there yet? [1]");
-while(answer.indexOf("yes") === -1) {
-    var answer = prompt("Are we there yet? [2]")
+var numAsks = 1;
+while(answer.indexOf("yes") === -1 && numAsks <=3) {
+    var answer = prompt("Are we there yet? [2]");
+    numAsks++;
 }
+alert("We made it!");
