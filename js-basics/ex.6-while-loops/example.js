3e5a58f5137104a01ffc29dcf9f3f18e0c89d3ee
Edilson Borges <edilsonfb@gmail.com>
2017-04-19 07:05:21 -0300

    Even, Odd and 5

--- js-basics/ex.6-while-loops/example.js
+++ js-basics/ex.6-while-loops/example.js
@@ -24,3 +24,30 @@ while (num <= 19) {
     console.log(num);
     num++;
 }
+
+console.log('Even numers');
+var num = 300;
+while (num <= 333) {
+    if (num % 2 !== 0) {
+        console.log(num);
+    }
+    num++;
+}
+
+console.log('Odd numers');
+var num = 10;
+while (num <= 40) {
+    if (num % 2 === 0) {
+        console.log(num);
+    }
+    num++;
+}
+
+console.log('Multiples of 5');
+var num = 5;
+while (num <= 50) {
+    if (num % 5 === 0) {
+        console.log(num);
+    }
+    num++;
+}
