16fcf2c14dbf780f33bd923ab79fac642ecfea45
Edilson Borges <edilsonfb@gmail.com>
2017-04-20 06:38:03 -0300

    For Loops Problem Set

--- js-basics/ex.7-for-loops/forLoops.js
+++ js-basics/ex.7-for-loops/forLoops.js
@@ -1,1 +1,22 @@
-alert('for loops!');
+for (num = -10; num <= 19; num++) {
+    console.log(num);
+}
+
+for (num = 10; num <= 40; num++) {
+    if (num % 2 === 0) {
+        console.log(num);
+    }
+}
+
+for (num = 300; num <= 333; num++) {
+    if (num % 2 !== 0) {
+        console.log(num);
+    }
+}
+
+for (num = 5; num <= 50; num++) {
+    if (num % 5 === 0) {
+        console.log(num);
+    }
+}
+
