cba859a9d84a1c29de0487e193291cabd5a4bd2a
Edilson Borges <edilsonfb@gmail.com>
2017-04-20 07:06:35 -0300

    Function with arguments

--- js-functions/ex.1- function-basics/example.js
+++ js-functions/ex.1- function-basics/example.js
@@ -2,7 +2,15 @@ function doSomething() {
     console.log("Hi!");
 }
 
+doSomething; // without parentheses this prints the funcion code
 doSomething();
 doSomething();
 doSomething();
-doSomething();
+
+function singSong(song) {
+    console.log(song);
+}
+
+singSong('test');
+singSong('test 2');
+singSong('test 3');
