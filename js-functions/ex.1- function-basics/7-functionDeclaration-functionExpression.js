8021adfd27bb159cfc0134ba7dcbf61059a93dff
Edilson Borges <edilsonfb@gmail.com>
2017-04-21 11:53:49 -0300

    Function Declaration and Function Expression

---
+++ js-functions/ex.1- function-basics/7-functionDeclaration-functionExpression.js
@@ -0,0 +1,9 @@
+//function declaration
+function capitalize(str) {
+    return str.charAt().toUpperCase() + str.slice(1);
+}
+
+//function expression
+var capitalize = function(str) {
+    return str.charAt().toUpperCase() + str.slice(1);
+}
