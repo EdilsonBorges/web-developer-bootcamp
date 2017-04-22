132f6c7f311c3384a672ff7b0ac6155634e4421a
Edilson Borges <edilsonfb@gmail.com>
2017-04-22 16:07:08 -0300

    Array create, Array length

--- js-arrays/ex.1-arrays/arrays.js
+++ js-arrays/ex.1-arrays/arrays.js
@@ -2,3 +2,11 @@ var colors = ['red','green','blue'];
 colors[10] = 'yellow';
 
 colors; // ["red", "green", "blue", undefined × 7, "yellow"]
+
+var ar = [];
+var ar = new Array();
+
+var randomCollection = [42,true,'harry',null];
+randomCollection; // [42, true, "harry", null]
+
+randomCollection.length; // 4
