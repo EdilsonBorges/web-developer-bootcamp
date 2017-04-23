e9832d10a172ccaa0a311428f1a7318dc0f42c16
Edilson Borges <edilsonfb@gmail.com>
2017-04-23 19:06:23 -0300

    Shift and Unshift

---
+++ js-arrays/ex.2-array-methods/2-shift-unshift.js
@@ -0,0 +1,7 @@
+var colors = ['red','orange','yellow','green'];
+
+colors.unshift('blue');
+//5 - ["blue", "red", "orange", "yellow", "green"]
+
+colors.shift();
+//blue - ["red", "orange", "yellow", "green"]
