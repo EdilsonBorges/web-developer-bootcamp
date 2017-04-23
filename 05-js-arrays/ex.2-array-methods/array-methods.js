aafd909182fe0adbdf929e04a21ca14bee21dcac
Edilson Borges <edilsonfb@gmail.com>
2017-04-23 19:22:17 -0300

    Array Quiz

--- 05-js-arrays/ex.2-array-methods/array-methods.js
+++ 05-js-arrays/ex.2-array-methods/array-methods.js
@@ -6,3 +6,15 @@ slice
 
 => https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array
 */
+
+var numbers = [22,67,33,96,88];
+console.log(numbers[numbers.length]);
+//-> undefined
+
+var friendGroups = [
+    ['Harry','Ron','Hermione'],
+    ['Malfoy','Crabbe','Goyle'],
+    ['Mooney','Wormtail','Prongs']
+];
+console.log(friendGroups[2][0]);
+//-> Mooney
