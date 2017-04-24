5e258ab58167f91f9779269c17377310726c2970
Edilson Borges <edilsonfb@gmail.com>
2017-04-24 07:10:51 -0300

    ForEach Quiz fix

--- 05-js-arrays/ex.4-array-iteration/example.js
+++ 05-js-arrays/ex.4-array-iteration/example.js
@@ -24,7 +24,7 @@ var numbers = [1,2,3,4,5,6,7,8,9,10];
 var colors = ['red','orange','yellow','green'];
 
 numbers.forEach(function(registry){
-    if(color % 3 === 0){
+    if(registry % 3 === 0){
         console.log(registry);
     }
 });
