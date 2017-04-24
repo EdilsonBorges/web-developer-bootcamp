d4bd33a0d2db8aa08cab6b914ce0b27c5e29e8c2
Edilson Borges <edilsonfb@gmail.com>
2017-04-24 06:54:12 -0300

    Array Iteration - ForEach

---
+++ 05-js-arrays/ex.4-array-iteration/example.js
@@ -0,0 +1,11 @@
+// For Loop
+var colors = ['red','green','blue'];
+for (var i = 0; i < colors.length; i++) {
+    console.log(colors[i]);
+}
+
+// ForEach arData.forEach(someFunction)
+var colors = ['red','green','blue'];
+colors.forEach(function(anyName){
+    console.log(anyName);
+});
