781e1323c0c3adaf28b9446923ad8fb537136342
Edilson Borges <edilsonfb@gmail.com>
2017-04-25 06:48:09 -0300

    Todos List 2 - using splice

--- 05-js-arrays/ex.5-todo-list-2/list.js
+++ 05-js-arrays/ex.5-todo-list-2/list.js
@@ -3,12 +3,17 @@ var input = prompt('What do you like to do?');
 
 while (input !== 'quit') {
     if (input == 'list') {
+        console.log("******");
         todos.forEach(function(todo, i){ //second argument is index
             console.log(i +": "+ todo);
         });
+        console.log("******");
     } else if (input == 'new') {
         var newTodo = prompt('Enter new Todo');
         todos.push(newTodo);
+    } else if (input == 'delete') {
+        var index = prompt('Enter index of todo to delete');
+        todos.splice(index,1);
     }
     var input = prompt('What do you like to do?');
 }
