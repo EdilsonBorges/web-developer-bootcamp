46b9fe761e7ebda1afdef79348249ac4bfdc334b
Edilson Borges <edilsonfb@gmail.com>
2017-04-25 06:54:16 -0300

    Todo List 2 - creating functions for actions

--- 05-js-arrays/ex.5-todo-list-2/list.js
+++ 05-js-arrays/ex.5-todo-list-2/list.js
@@ -3,18 +3,30 @@ var input = prompt('What do you like to do?');
 
 while (input !== 'quit') {
     if (input == 'list') {
+        listTodos();
+    } else if (input == 'new') {
+        addTodo();
+    } else if (input == 'delete') {
+        deleteTodo();
+    }
+    var input = prompt('What do you like to do?');
+}
+console.log('Thank you for using the system');
+
+function listTodos(){
         console.log("******");
         todos.forEach(function(todo, i){ //second argument is index
             console.log(i +": "+ todo);
         });
         console.log("******");
-    } else if (input == 'new') {
+}
+
+function addTodo(){
         var newTodo = prompt('Enter new Todo');
         todos.push(newTodo);
-    } else if (input == 'delete') {
+}
+
+function deleteTodo(){
         var index = prompt('Enter index of todo to delete');
         todos.splice(index,1);
-    }
-    var input = prompt('What do you like to do?');
 }
-console.log('Thank you for using the system');
