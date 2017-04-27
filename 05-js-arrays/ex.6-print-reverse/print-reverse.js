e271ade40bcfeaa410ca1f64bd98948795879e14
Edilson Borges <edilsonfb@gmail.com>
2017-04-26 21:37:57 -0300

    Array Problem Set

--- 05-js-arrays/ex.6-print-reverse/print-reverse.js
+++ 05-js-arrays/ex.6-print-reverse/print-reverse.js
@@ -4,3 +4,35 @@ function printReverse (datas) {
     }
 }
 printReverse([1,2,3,4]);
+
+function isUniform(data) {
+    var number = data[0];
+    for(i = 0; i < data.length;i++){
+        if(number != data[i]) {
+            return false;
+        }
+        number = data[i];
+    }
+    return true;
+}
+isUniform(['1','1','1','1']);
+
+function sumArray(data) {
+    var sum = 0;
+    for(i = 0; i< data.length; i++) {
+        sum = sum + data[i];
+    }
+    return sum;
+}
+sumArray([1,2,3,4,5]);
+
+function max(data) {
+    number = 0;
+    for(i = 0; i < data.length;i++){
+        if(number < data[i]) {
+            number = data[i];
+        }
+    }
+    return number;
+}
+max([1,2,3]);
