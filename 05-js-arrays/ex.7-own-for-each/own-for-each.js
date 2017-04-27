c4d52313bf5833f36721e0d06ec1a0ff500d70a8
Edilson Borges <edilsonfb@gmail.com>
2017-04-27 07:13:57 -0300

    Own For Each - pt 3

--- 05-js-arrays/ex.7-own-for-each/own-for-each.js
+++ 05-js-arrays/ex.7-own-for-each/own-for-each.js
@@ -7,8 +7,15 @@ function ownForEach(arr, func){
     // loop through array
     for(i = 0; i < arr.length; i++){
         // call func for each item in array
-        func();
+        func(arr[i]);
     }
 }
 var colors = ['red','orange','yellow'];
-ownForEach(colors, alert);
+
+ownForEach(colors, function(color){
+  console.log(color);
+});
+
+
+//function that i'll be executed just once:
+(function(){console.log("Im a function")})()
