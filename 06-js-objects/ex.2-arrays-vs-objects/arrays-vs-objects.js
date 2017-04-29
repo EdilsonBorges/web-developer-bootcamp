09349edc13a9902be66656ca25192bf6d15acc50
Edilson Borges <edilsonfb@gmail.com>
2017-04-28 21:41:32 -0300

    JS Arrays vs Objects

---
+++ 06-js-objects/ex.2-arrays-vs-objects/arrays-vs-objects.js
@@ -0,0 +1,18 @@
+var dogs = ['Rusty','Lucky','Bubba'];
+dogs;
+dogs[1];
+dogs.push('Wyatt');
+dogs[1] = 'Lucy';
+//------------------------------------------
+
+
+var dog = {
+    name: 'Bubba',
+    breed: 'Lab'
+}
+dog;
+dog['name'];
+dog.age = 6;
+dog.breed = 'Black Lab';
+//------------------------------------------
+
