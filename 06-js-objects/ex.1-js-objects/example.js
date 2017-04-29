43703cd468823da14a240a2060ab06fc0a4bf198
Edilson Borges <edilsonfb@gmail.com>
2017-04-28 21:34:41 -0300

    JS Objects basic examples

--- 06-js-objects/ex.1-js-objects/example.js
+++ 06-js-objects/ex.1-js-objects/example.js
@@ -1,5 +1,19 @@
 var person = {
     name: "Cindy",
-    age: "32",
+    age: 32,
     city: "Missoula"
 };
+
+console.log(person['name']);
+console.log(person.nome);
+
+person['age'] += 1;
+person.city = 'london';
+
+var person2 = {};
+person2.name = 'travis';
+person2.job = 'tester';
+
+var person3 = new Object();
+person3.name = 'travis';
+person3.job = 'tester';
