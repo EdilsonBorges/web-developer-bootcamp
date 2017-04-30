9ee1471bedb259102876ba81162f768840daa187
Edilson Borges <edilsonfb@gmail.com>
2017-04-29 23:20:11 -0300

    Ex 4 - Movie DB

---
+++ 06-js-objects/ex.4-movie-db/script.js
@@ -0,0 +1,18 @@
+var movies = [
+    {
+        title: 'In Bruges',
+        stars: 5
+    },
+    {
+        title: 'Frozen',
+        stars: 4.5
+    },
+    {
+        title: 'Mad Max Fury Road',
+        stars: 5
+    },
+    {
+        title: 'Les Miserables',
+        stars: 3.5
+    }
+];
