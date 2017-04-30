997274e5f5abb088752e7812d6143d847a08492c
Edilson Borges <edilsonfb@gmail.com>
2017-04-30 18:44:59 -0300

    Animating with dom manipulation

--- 06-js-objects/ex.6-dom-manipulation/script.js
+++ 06-js-objects/ex.6-dom-manipulation/script.js
@@ -1,2 +1,13 @@
 var h1 = document.querySelector('h1');
 h1.style.color = 'pink';
+
+var body = document.querySelector('body');
+setInterval(function(){
+        if(body.style.background == 'blue'){
+            body.style.background = 'green'
+        } else {
+            body.style.background = 'blue'
+        }
+    }
+    ,1000)
+// clearInterval(1);
