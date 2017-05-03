0660562239f07333d72c939dff70aa9f392ec605
Edilson Borges <edilsonfb@gmail.com>
2017-05-03 07:08:52 -0300

    Using negative (!variable) to do the toggling

---
+++ 08-advanced-dom-manipulation/ex.2-color-toggle-exercise/script.js
@@ -0,0 +1,23 @@
+var tag = document.querySelector('p');
+var button = document.querySelector('button');
+var isPurple = false;
+
+button.addEventListener('click',changeBackground);
+button.addEventListener('click',changeText);
+
+function changeBackground() {
+    if (isPurple) {
+        document.body.style.background = 'white';
+    } else {
+        document.body.style.background = 'purple';
+    }
+    isPurple = !isPurple;
+}
+
+function changeText(){
+    if (tag.textContent == 'Not clicked') {
+        tag.textContent = 'Clicked';
+    } else {
+        tag.textContent = 'Not clicked';
+    }
+}
