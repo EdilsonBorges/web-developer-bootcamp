198f5e1d991904541de13ca5257b3347ced2e9e1
Edilson Borges <edilsonfb@gmail.com>
2017-05-04 07:15:43 -0300

    Score keeper pt.3

--- 08-advanced-dom-manipulation/ex.3-score-keeper/script.js
+++ 08-advanced-dom-manipulation/ex.3-score-keeper/script.js
@@ -1,15 +1,24 @@
 var p1Button = document.querySelector('#p1');
 var p2Button = document.querySelector('#p2');
 var reset = document.querySelector('#reset');
-var h1 = document.querySelector('h1');
+var p1Display = document.querySelector('#p1Display');
+var p2Display = document.querySelector('#p2Display');
 var p1Score = 0;
 var p2Score = 0;
 
 p1Button.addEventListener('click',function (){
     p1Score++;
-
-})
+    p1Display.textContent = p1Score;
+});
 
 p2Button.addEventListener('click',function (){
     p2Score++;
-})
+    p2Display.textContent = p2Score;
+});
+
+reset.addEventListener('click',function (){
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = p1Score;
+    p2Display.textContent = p2Score;
+});
