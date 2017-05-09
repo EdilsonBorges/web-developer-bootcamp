bf961e1ada8d491bf06a9e843fcd76ea9fa2bc93
Edilson Borges <edilsonfb@gmail.com>
2017-05-09 06:37:11 -0300

    Changing h1 color when correct

--- 09-color-game-project/script.js
+++ 09-color-game-project/script.js
@@ -1,10 +1,10 @@
 var colors = generateRandomColors(6);
 var squares = document.querySelectorAll('.square');
 var pickedColor = pickColor();
-
-
 var span = document.querySelector('span');
 var messageDisplay = document.querySelector('#message');
+var h1 = document.querySelector('h1');
+
 span.textContent = pickedColor;
 
 for(i = 0; i < squares.length; i++){
@@ -12,6 +12,7 @@ for(i = 0; i < squares.length; i++){
     squares[i].addEventListener('click',function(){
         if(this.style.background === pickedColor){
             messageDisplay.textContent = 'Correct!';
+            h1.style.background = pickedColor;
             changeColors(pickedColor);
         } else {
             this.style.background = '#232323';
@@ -35,9 +36,9 @@ function generateRandomColors(num) {
     var arr = [];
     for(i = 0; i < num; i++) {
         arr.push('rgb('+
-        Math.floor(Math.random() * 255)+', '+
-        Math.floor(Math.random() * 255)+', '+
-        Math.floor(Math.random() * 255)+')');
+        Math.floor(Math.random() * 256)+', '+
+        Math.floor(Math.random() * 256)+', '+
+        Math.floor(Math.random() * 256)+')');
     }
     return arr;
 }
