03f0d4355d005d83aaedd7aefa0dc09c9684da0d
Edilson Borges <edilsonfb@gmail.com>
2017-05-11 06:47:21 -0300

    Creating functions following the refactoring

--- 09-color-game-project/script.js
+++ 09-color-game-project/script.js
@@ -1,21 +1,47 @@
 var numSquares = 6;
-var colors = generateRandomColors(numSquares);
+var colors = [];
+var pickedColor;
 var squares = document.querySelectorAll('.square');
-var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.querySelector('#message');
 var h1 = document.querySelector('h1');
 var resetButton = document.querySelector('#reset');
 var modeButtons = document.querySelectorAll('.mode');
 
-for(i = 0; i < modeButtons.length; i++) {
-    modeButtons[i].addEventListener('click',function(){
-    modeButtons[0].classList.remove('selected');
-    modeButtons[1].classList.remove('selected');
-    this.classList.add('selected');
-    this.textContent === 'Easy' ? numSquares = 3: numSquares = 6;
+init();
+
+function init() {
+    setupModeButtons();
+    setupSquare();
     reset();
-    });
+}
+
+function setupModeButtons() {
+    for(i = 0; i < modeButtons.length; i++) {
+    modeButtons[i].addEventListener('click',function() {
+        modeButtons[0].classList.remove('selected');
+        modeButtons[1].classList.remove('selected');
+        this.classList.add('selected');
+        this.textContent === 'Easy' ? numSquares = 3: numSquares = 6;
+        reset();
+        });
+    }
+}
+
+function setupSquare() {
+    for(i = 0; i < squares.length; i++){
+        squares[i].addEventListener('click',function(){
+            if(this.style.background === pickedColor){
+                messageDisplay.textContent = 'Correct!';
+                resetButton.textContent = 'Play Again?';
+                h1.style.background = pickedColor;
+                changeColors(pickedColor);
+            } else {
+                this.style.background = '#232323';
+                messageDisplay.textContent = 'Try Again';
+            }
+        });
+    }
 }
 
 function reset() {
@@ -38,23 +64,6 @@ function reset() {
 resetButton.addEventListener('click',function(){
     reset();
 })
-
-colorDisplay.textContent = pickedColor;
-
-for(i = 0; i < squares.length; i++){
-    squares[i].style.background = colors[i];
-    squares[i].addEventListener('click',function(){
-        if(this.style.background === pickedColor){
-            messageDisplay.textContent = 'Correct!';
-            resetButton.textContent = 'Play Again?';
-            h1.style.background = pickedColor;
-            changeColors(pickedColor);
-        } else {
-            this.style.background = '#232323';
-            messageDisplay.textContent = 'Try Again';
-        }
-    });
-}
 
 function changeColors(color) {
     for(i = 0; i < squares.length; i++) {
