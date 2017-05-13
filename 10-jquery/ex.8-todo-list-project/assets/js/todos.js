3dc346b115d46a9bdc3cc493c2dd533694f6a9bd
Edilson Borges <edilsonfb@gmail.com>
2017-05-13 16:19:08 -0300

    Shortening js with css class

--- 10-jquery/ex.8-todo-list-project/assets/js/todos.js
+++ 10-jquery/ex.8-todo-list-project/assets/js/todos.js
@@ -1,18 +1,10 @@
-// check off
 
 $('li').on('click',function(){
-    // if li is gray -> turn black
-    console.log($(this).css('color') )
-    if($(this).css('color') === 'rgb(0, 0, 0)'){
-        $(this).css({
-            color: 'gray',
-            textDecoration: 'line-through'
-        });
-    } else {
-        $(this).css({
-            color: 'black',
-            textDecoration: 'none'
-        });
-    }
-    
+    $(this).toggleClass('completed');
 })
+
+$('span').on('click',function(){
+    $(this).toggleClass('removed');
+})
+
+//avoid event bubbling
