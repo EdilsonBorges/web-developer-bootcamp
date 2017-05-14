06e723aae6b33b4989da8d20c06aa868dc1c0182
Edilson Borges <edilsonfb@gmail.com>
2017-05-14 12:24:35 -0300

    Creating new itens on list

--- 10-jquery/ex.8-todo-list-project/assets/js/todos.js
+++ 10-jquery/ex.8-todo-list-project/assets/js/todos.js
@@ -1,12 +1,22 @@
 
 $('li').on('click',function(){
     $(this).toggleClass('completed');
-})
+});
 
 $('span').on('click',function(e){
-    $(this).parent().toggleClass('removed');
+    // $(this).parent().toggleClass('removed');
+    $(this).parent().fadeOut(300,function(){
+        $(this).toggleClass('removed');
+    })
     e.stopPropagation();
-})
-
+});
 //avoid event bubbling with stopPropagation
 //include all li to hide with parent()
+
+$("input[type='text']").keypress(function(e){
+    if (e.which === 13) {
+        var todoText = $(this).val();
+        $(this).val('');
+        $('ul').append('<li>'+todoText+'</li:>');
+    }
+})
