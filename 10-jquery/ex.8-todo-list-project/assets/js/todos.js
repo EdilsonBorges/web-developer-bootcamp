500b9066643b0db786331e6e100f26b428f4bd73
Edilson Borges <edilsonfb@gmail.com>
2017-05-15 07:06:25 -0300

    Animating trash-cam, getting rid of input border

--- 10-jquery/ex.8-todo-list-project/assets/js/todos.js
+++ 10-jquery/ex.8-todo-list-project/assets/js/todos.js
@@ -17,6 +17,6 @@ $("input[type='text']").keypress(function(e){
     if (e.which === 13 && $(this).val() !== '') {
         var todoText = $(this).val();
         $(this).val('');
-        $('ul').append('<li><span>X</span> '+todoText+'</li:>');
+        $('ul').append('<li><span><i class=\'fa fa-trash\'></i></span> '+todoText+'</li:>');
     }
 })
