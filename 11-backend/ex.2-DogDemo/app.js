658e2915b40fb8cd7899ca7625efcc7e54b2b966
Edilson Borges <edilsonfb@gmail.com>
2017-05-24 23:49:50 -0300

    Post request

--- 11-backend/ex.2-DogDemo/app.js
+++ 11-backend/ex.2-DogDemo/app.js
@@ -1,3 +1,12 @@
 app.get("/", function(){
     res.render("home")
 });
+
+app.post("/createDog",function(req, res){
+    Dog.create({
+        name: req.body.name,
+        breed: req.body.breed
+    }, function(err, dog){
+        res.redirect("/dogs");
+    });
+});
