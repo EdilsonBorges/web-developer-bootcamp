24c63d536ac7fa21499770ae12892b07b676c6ca
Edilson Borges <edilsonfb@gmail.com>
2017-05-26 23:47:58 -0300

    Testing output, organizing files

--- 11-backend/ex.2-intro-node/exercise2/exercise2.js
+++ 11-backend/ex.2-intro-node/exercise2/exercise2.js
@@ -1,7 +1,9 @@
 var grader = require('./grader.js');
 
+console.log('Score 1: ');
 var scores = [90,98,89,100,100,86,94];
 console.log(grader.average(scores));
 
+console.log('Score 2: ');
 var scores2 = [40,65,77,82,80,54,73,63,95,49];
 console.log(grader.average(scores2));
