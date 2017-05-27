90d443a1c4abb4e309e9b8cafda5c3af1445617a
Edilson Borges <edilsonfb@gmail.com>
2017-05-26 23:45:36 -0300

    exporting method of grader.js and Including/calling into exercise2's

--- 11-backend/ex.2-intro-node/exercise2.js
+++ 11-backend/ex.2-intro-node/exercise2.js
@@ -1,5 +1,7 @@
+var grader = require('./grader.js');
+
 var scores = [90,98,89,100,100,86,94];
-average(scores);
+console.log(grader.average(scores));
 
 var scores2 = [40,65,77,82,80,54,73,63,95,49];
-average(scores2);
+console.log(grader.average(scores2));
--- 11-backend/ex.2-intro-node/grader.js
+++ 11-backend/ex.2-intro-node/grader.js
@@ -1,19 +1,10 @@
-function average(scores) {
-    //add all scores together
-    var total = 0;
-    scores.forEach(function(score){
-        total +=score;
-    });
-    //divide by total
-    var avg = total/scores.length;
-    //round avg
-    return Math.round(avg);
+module.exports = {
+    average: function(scores) {
+        var total = 0;
+        scores.forEach(function(score){
+            total +=score;
+        });
+        var avg = total/scores.length;
+        return Math.round(avg);
+    }
 }
-
-console.log('1 - ');
-var scores = [90,98,89,100,100,86,94];
-console.log(average(scores));
-
-console.log('; 2 - ');
-var scores2 = [40,65,77,82,80,54,73,63,95,49];
-console.log(average(scores2));
