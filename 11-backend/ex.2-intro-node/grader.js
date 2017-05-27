caee719902b60fb2e8b90d236cc0cc2f78156f06
Edilson Borges <edilsonfb@gmail.com>
2017-05-26 23:42:06 -0300

    testing out

--- 11-backend/ex.2-intro-node/grader.js
+++ 11-backend/ex.2-intro-node/grader.js
@@ -10,8 +10,10 @@ function average(scores) {
     return Math.round(avg);
 }
 
-// var scores = [90,98,89,100,100,86,94];
-// average(scores);
+console.log('1 - ');
+var scores = [90,98,89,100,100,86,94];
+console.log(average(scores));
 
-// var scores2 = [40,65,77,82,80,54,73,63,95,49];
-// average(scores2);
+console.log('; 2 - ');
+var scores2 = [40,65,77,82,80,54,73,63,95,49];
+console.log(average(scores2));
