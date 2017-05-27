49119a998bf66b0458532966d703a2b7e1155c32
Edilson Borges <edilsonfb@gmail.com>
2017-05-27 19:48:04 -0300

    lower function

--- 11-backend/ex.2-intro-node/exercise3/lower.js
+++ 11-backend/ex.2-intro-node/exercise3/lower.js
@@ -1,1 +1,9 @@
-alert('connected!');
+function lower(scores) {
+    var actual = 0;
+    scores.forEach(function(score){
+        if(actual < score) {
+            result = score;
+        }
+    });
+    return result;
+}
