e4d9c949758ca51b7e13b056041d94d8b016efee
Edilson Borges <edilsonfb@gmail.com>
2017-05-27 19:45:03 -0300

    Setting up scores

--- 11-backend/ex.2-intro-node/exercise3/exercise3.js
+++ 11-backend/ex.2-intro-node/exercise3/exercise3.js
@@ -1,1 +1,7 @@
 var lower = require('./lower.js');
+
+var scores = [5,8,44,12,816,516,1];
+console.log(grader.lower(scores));
+
+var scores2 = [0,84,96,46,5,159,8];
+console.log(grader.lower(scores2));
