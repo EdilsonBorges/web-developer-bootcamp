ccba7d7efd406650fd0165281ebc984c95eff68d
Edilson Borges <edilsonfb@gmail.com>
2017-05-27 19:50:36 -0300

    testing other values

--- 11-backend/ex.2-intro-node/exercise3/exercise3.js
+++ 11-backend/ex.2-intro-node/exercise3/exercise3.js
@@ -1,7 +1,9 @@
 var lower = require('./lower.js');
 
-var scores = [5,8,44,12,816,516,1];
+console.log('Score 1: ');
+var scores = [5,8,44,16,19,11,15,12,816,516,1];
 console.log(grader.lower(scores));
 
-var scores2 = [0,84,96,46,5,159,8];
+console.log('Score 2: ');
+var scores2 = [05,81,156,15,84,96,46,5,159,8];
 console.log(grader.lower(scores2));
