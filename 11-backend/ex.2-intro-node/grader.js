1ed3e77e454cb78950f65b369142c608b4c78a9f
Edilson Borges <edilsonfb@gmail.com>
2017-05-26 23:40:38 -0300

    Logic of the method

--- 11-backend/ex.2-intro-node/grader.js
+++ 11-backend/ex.2-intro-node/grader.js
@@ -1,7 +1,13 @@
 function average(scores) {
     //add all scores together
+    var total = 0;
+    scores.forEach(function(score){
+        total +=score;
+    });
     //divide by total
+    var avg = total/scores.length;
     //round avg
+    return Math.round(avg);
 }
 
 // var scores = [90,98,89,100,100,86,94];
