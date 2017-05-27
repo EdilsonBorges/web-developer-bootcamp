f9bd3ebce9c3d2ef3f3efbb9a41b963cc0eeeb6a
Edilson Borges <edilsonfb@gmail.com>
2017-05-27 19:48:16 -0300

    exporting lower function

--- 11-backend/ex.2-intro-node/exercise3/lower.js
+++ 11-backend/ex.2-intro-node/exercise3/lower.js
@@ -1,9 +1,11 @@
-function lower(scores) {
-    var actual = 0;
-    scores.forEach(function(score){
-        if(actual < score) {
-            result = score;
-        }
-    });
-    return result;
+module.exports = {
+    lower: function(scores) {
+        var actual = 0;
+        scores.forEach(function(score){
+            if(actual < score) {
+                result = score;
+            }
+        });
+        return result;
+    }
 }
