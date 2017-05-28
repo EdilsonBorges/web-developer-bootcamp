c9b83b758f5fbeffcf566cb0354b477e5015df0d
Edilson Borges <edilsonfb@gmail.com>
2017-05-28 12:15:04 -0300

    Npm

--- 11-backend/ex.2-node/exercise3/exercise3.js
+++ 11-backend/ex.2-node/exercise3/exercise3.js
@@ -1,9 +1,9 @@
-var lower = require('./lower.js');
+var lowerClass = require('./lower.js');
 
 console.log('Score 1: ');
 var scores = [5,8,44,16,19,11,15,12,816,516,1];
-console.log(grader.lower(scores));
+console.log(lowerClass.lower(scores));
 
 console.log('Score 2: ');
 var scores2 = [05,81,156,15,84,96,46,5,159,8];
-console.log(grader.lower(scores2));
+console.log(lowerClass.lower(scores2));
---
+++ 11-backend/ex.3-npm/demoApp/app.js
@@ -0,0 +1,14 @@
+/*
+- intro npm
+- why npm
+- packages
+*/
+
+/*
+npm install
+require()
+
+npm install cat-me
+*/
+
+console.log("from app.js");
