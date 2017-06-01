43eae0634ba0df580413b90a91b03df07e7ea079
Edilson Borges <edilsonfb@gmail.com>
2017-05-31 22:32:09 -0300

    Ejs looping

--- 11-backend/ex.4-express/ex.2-ejs-demo/app.js
+++ 11-backend/ex.4-express/ex.2-ejs-demo/app.js
@@ -11,6 +11,16 @@ app.get('/love/:thing/:id', function(req, res){
     res.render('love.ejs', {thingVar: thing, idVar: id})
 })
 
+app.get('/posts', function(req, res){
+    var posts = [
+        {title: 'Post 1', author: 'Author 1'},
+        {title: 'Post 2', author: 'Author 2'},
+        {title: 'Post 3', author: 'Author 3'},
+    ]
+
+    res.render('posts.ejs', {posts: posts})
+})
+
 app.listen('3000', function(){
     console.log('Server is listening')
 })
