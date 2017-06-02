3c77e11f00816a3d8e2815668bac9fc01e8e896c
Edilson Borges <edilson.borges@agrodefesa.go.gov.br>
2017-06-02 13:14:42 -0300

    Creating view to display titles

--- 12-api/ex.4-movie-search/app.js
+++ 12-api/ex.4-movie-search/app.js
@@ -1,12 +1,12 @@
 var app = require('express')()
 var request = require('request')
+app.set('view engine', 'ejs')
 
 app.get('/results', function(req, res){
-    request('http://www.omdbapi.com/?s=california&apikey=thewdb', function(error, response, body){
+    request('http://www.omdbapi.com/?s=how&apikey=thewdb', function(error, response, body){
         if (!error &&response.statusCode == 200 ) {
-                var results = JSON.parse(body)
-
-                res.send(results['Search'][0]['Title'])
+                var data = JSON.parse(body)
+                res.render('results', {data: data})
         }
     })
 })
