6c302a01e3f2570ca3a8f3de07ab73c3050c378a
Edilson Borges <edilson.borges@agrodefesa.go.gov.br>
2017-06-02 13:07:00 -0300

    Movie Search API - Inclusing express - making route results - requesting api omdb - showing first movie title

--- 12-api/ex.4-movie-search/app.js
+++ 12-api/ex.4-movie-search/app.js
@@ -1,10 +1,16 @@
-var request = require('request');
-request('http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb', function(error, response, body){
-    if(error){
-        console.log('something went wrong: '+error)
-    } else {
-        if(response.statusCode == 200){
-            console.log(body)
+var app = require('express')()
+var request = require('request')
+
+app.get('/results', function(req, res){
+    request('http://www.omdbapi.com/?s=california&apikey=thewdb', function(error, response, body){
+        if (!error &&response.statusCode == 200 ) {
+                var results = JSON.parse(body)
+
+                res.send(results['Search'][0]['Title'])
         }
-    }
+    })
+})
+
+app.listen('3000', function(){
+    console.log('server started')
 })
