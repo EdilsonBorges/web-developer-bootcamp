0d3885b3e701972322d2cb1e3803c47e504908b7
Edilson Borges <edilson.borges@agrodefesa.go.gov.br>
2017-06-02 13:25:51 -0300

    Movie Search API: creating search

--- 12-api/ex.4-movie-search/app.js
+++ 12-api/ex.4-movie-search/app.js
@@ -2,8 +2,14 @@ var app = require('express')()
 var request = require('request')
 app.set('view engine', 'ejs')
 
+app.get('/', function(req, res){
+    res.render('search')
+})
+
 app.get('/results', function(req, res){
-    request('http://www.omdbapi.com/?s=how&apikey=thewdb', function(error, response, body){
+    var term = req.query.search
+    var url = 'http://www.omdbapi.com/?s='+term+'&apikey=thewdb';
+    request(url, function(error, response, body){
         if (!error &&response.statusCode == 200 ) {
                 var data = JSON.parse(body)
                 res.render('results', {data: data})
