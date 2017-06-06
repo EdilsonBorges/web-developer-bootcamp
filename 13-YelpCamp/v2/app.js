9014834a5030270b5830de355fcc3e00f7a574f7
Edilson Borges <edilsonfb@gmail.com>
2017-06-05 22:53:13 -0300

    Persisting camps on db

--- 13-YelpCamp/v2/app.js
+++ 13-YelpCamp/v2/app.js
@@ -6,7 +6,6 @@ mongoose.connect('mongodb://localhost/yelp_camp')
 app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 
-// schema setup
 var campgroundSchema = new mongoose.Schema({
     name: String,
     image: String
@@ -14,10 +13,12 @@ var campgroundSchema = new mongoose.Schema({
 
 var Campground = mongoose.model('Campground', campgroundSchema)
 
-// Campground.create({
-//     name: 'Camp 1',
-//     image: 'http://nationalhomegrantfoundation.com/wp-content/uploads/2015/12/Scout-Camp.jpg'
-// }, function(err, campground){
+// Campground.create(
+//     { name: 'Camp 1', image: 'http://nationalhomegrantfoundation.com/wp-content/uploads/2015/12/Scout-Camp.jpg' },
+//     { name: 'Camp 2', image: 'http://www.travelbirbilling.com/wp-content/uploads/camp-pic1.jpg' },
+//     { name: 'Camp 3', image: 'http://blog.via.com/wp-content/uploads/2015/12/Thanedar-Himachal-Pradesh.jpg' },
+//     { name: 'Camp 4', image: 'https://media-cdn.tripadvisor.com/media/photo-s/0a/88/1c/a1/camp-oak-view.jpg' }
+//     , function(err, campground){
 //     if(err){
 //         console.log(err)
 //     } else {
@@ -26,46 +27,31 @@ var Campground = mongoose.model('Campground', campgroundSchema)
 //     }
 // })
 
-// var campgrounds = [
-//     { name: 'Camp 1', image: 'http://nationalhomegrantfoundation.com/wp-content/uploads/2015/12/Scout-Camp.jpg' },
-//     { name: 'Camp 2', image: 'http://www.travelbirbilling.com/wp-content/uploads/camp-pic1.jpg' },
-//     { name: 'Camp 3', image: 'http://blog.via.com/wp-content/uploads/2015/12/Thanedar-Himachal-Pradesh.jpg' },
-//     { name: 'Camp 4', image: 'https://media-cdn.tripadvisor.com/media/photo-s/0a/88/1c/a1/camp-oak-view.jpg' },
-//     { name: 'Camp 1', image: 'http://nationalhomegrantfoundation.com/wp-content/uploads/2015/12/Scout-Camp.jpg' },
-//     { name: 'Camp 2', image: 'http://www.travelbirbilling.com/wp-content/uploads/camp-pic1.jpg' },
-//     { name: 'Camp 3', image: 'http://blog.via.com/wp-content/uploads/2015/12/Thanedar-Himachal-Pradesh.jpg' },
-//     { name: 'Camp 4', image: 'https://media-cdn.tripadvisor.com/media/photo-s/0a/88/1c/a1/camp-oak-view.jpg' }
-// ]
-
 app.get('/', function(req, res){
     res.render('landing')
 })
 
 app.get('/campgrounds', function(req, res){
     Campground.find({}, function(err, allcampgrounds){
-    if(err){
-        console.log(err)
-    } else {
-        res.render('campgrounds', {campgrounds: allcampgrounds})
-    }
-})
-    //get all data from db
+        if(err){
+            console.log(err)
+        } else {
+            res.render('campgrounds', {campgrounds: allcampgrounds})
+        }
+    })
 })
 
 app.post('/campgrounds', function(req, res){
     var name = req.body.name
     var image = req.body.image
     var newCampground = {name: name, image: image}
-
     Campground.create(newCampground, function(err, newlyCreated){
-    if(err){
-        console.log(err)
-    } else {
-        res.render('campgrounds', {campgrounds: newlyCreated})
-        res.redirect('/campgrounds')
-    }
-})
-
+        if(err){
+            console.log(err)
+        } else {
+            res.redirect('/campgrounds')
+        }
+    })
 })
 
 app.get('/campgrounds/new', function(req, res){
