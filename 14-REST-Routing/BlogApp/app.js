0608b723b2fa3ce88ad71bae7839b6c05712c4dd
Edilson Borges <edilsonfb@gmail.com>
2017-06-07 23:18:55 -0300

    Creating schema for blog

--- 14-REST-Routing/BlogApp/app.js
+++ 14-REST-Routing/BlogApp/app.js
@@ -3,11 +3,22 @@ var express = require('express')
     mongoose = require('mongoose')
     app = express()
 
+// App config
 mongoose.connect('mongodb://localhost/blog_app')
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
 
+// Mongoose/Model Config
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+})
+var Blog = mongoose.model('Blog', blogSchema)
+
+// RESTful Routes
 app.listen('3000', function(){
     console.log('Server Running')
 })
