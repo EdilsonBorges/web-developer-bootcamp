88b4833fb8e04ff6e20947601d9ca1fea4bee551
Edilson Borges <edilsonfb@gmail.com>
2017-06-12 23:56:31 -0300

    New files for some references.js function

--- 15-Data-Associations/references.js
+++ 15-Data-Associations/references.js
@@ -1,34 +1,21 @@
-var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/blog_demo_2')
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/blog_demo_2');
 
-// POST - title, content
-var postSchema = new mongoose.Schema({
-    title: String,
-    content: String
-})
-var Post = mongoose.model("Post", postSchema)
+var Post = require('./models/post');
+var User = require('./models/user');
 
-// USER - email, name
-var userSchema = new mongoose.Schema({
-    email: String,
-    name: String,
-    posts: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Post'
-        }
-    ]
-})
-var User = mongoose.model("User", userSchema)
+// Creating user:
 
 // User.create({
 //     email: 'bob@gmail.com',
 //     name: 'Bob Belcher'
 // })
 
+// Creating post:
+
 // Post.create({
-//     title: 'How to cook the best burger pt.3',
-//     content: 'Test some content'
+//     title: 'How to cook the best burger pt.4',
+//     content: 'Test!!'
 // }, function(err, post){
 //     User.findOne({email: 'bob@gmail.com'}, function(err, foundUser){
 //         if(err){
@@ -45,6 +32,8 @@ var User = mongoose.model("User", userSchema)
 //         }
 //     })
 // })
+
+// Finding registry
 
 User.findOne({email:'bob@gmail.com'}).populate('posts').exec(function(err, user){
     if(err){
