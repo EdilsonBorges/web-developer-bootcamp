708e734370110183eddd68e29545ee81e40964f0
Edilson Borges <edilsonfb@gmail.com>
2017-06-18 20:25:55 -0300

    Required libs

--- 16-Authentication/AuthDemo/app.js
+++ 16-Authentication/AuthDemo/app.js
@@ -1,7 +1,12 @@
-var express = require("express");
-var app = express();
-var mongoose = require('mongoose');
+var express = require("express"),
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    bodyParser = require('body-parser'),
+    LocalStrategy =  require('passport-local'),
+    passportLocalMongoose =  require('passport-local-mongoose');
+
 mongoose.connect('mongodb://localhost/auth_demo_app');
+var app = express();
 
 app.set('view engine', 'ejs');
 
