f1a439ab7259657c816661e89614f9a7ff840cd2
Edilson Borges <edilsonfb@gmail.com>
2017-06-18 20:37:05 -0300

    Require User file

--- 16-Authentication/AuthDemo/app.js
+++ 16-Authentication/AuthDemo/app.js
@@ -2,12 +2,13 @@ var express = require("express"),
     mongoose = require('mongoose'),
     passport = require('passport'),
     bodyParser = require('body-parser'),
+    User = require('./models/user'),
     LocalStrategy =  require('passport-local'),
     passportLocalMongoose =  require('passport-local-mongoose');
 
 mongoose.connect('mongodb://localhost/auth_demo_app');
-var app = express();
 
+var app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res){
