4516afb18f103ce4b57028b9c45408b505b69756
Edilson Borges <edilsonfb@gmail.com>
2017-06-19 23:03:40 -0300

    new route

--- 16-Authentication/AuthDemo/app.js
+++ 16-Authentication/AuthDemo/app.js
@@ -21,8 +21,16 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//========
+// ROUTES
+//========
+
 app.get('/', function (req, res){
     res.render('home');
+});
+
+app.get('/secret', function (req, res){
+    res.render('secret');
 });
 
 app.listen('3000', function(){
