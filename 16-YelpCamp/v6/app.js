2834310b915cecda241c02d8d25d85ebfb0cc2fc
Edilson Borges <edilsonfb@gmail.com>
2017-06-20 23:45:11 -0300

    Passport configuration

--- 16-YelpCamp/v6/app.js
+++ 16-YelpCamp/v6/app.js
@@ -15,6 +15,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname+'/public'));
 app.set('view engine', 'ejs');
 
+// PASSPORT CONFIGURATION
+app.use(require('express-session')({
+    secret: "Just some random text again",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 app.get('/', function(req, res){
     res.render('landing')
 });
