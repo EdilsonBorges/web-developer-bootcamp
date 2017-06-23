32d8986c82758e1b725abcb1cbe9735e5d47804c
Edilson Borges <edilsonfb@gmail.com>
2017-06-23 07:28:24 -0300

    Route name setup on app.js

--- 16-YelpCamp/v6/routes/campgrounds.js
+++ 16-YelpCamp/v6/routes/campgrounds.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Campground = require('../models/campground');
 
-router.get('/campgrounds', function(req, res){
+router.get('/', function(req, res){
     Campground.find({}, function(err, allcampgrounds){
         if(err){
             console.log(err)
@@ -12,7 +12,7 @@ router.get('/campgrounds', function(req, res){
     })
 });
 
-router.post('/campgrounds', function(req, res){
+router.post('/', function(req, res){
     var name = req.body.name
     var image = req.body.image
     var desc = req.body.description
@@ -26,11 +26,11 @@ router.post('/campgrounds', function(req, res){
     })
 });
 
-router.get('/campgrounds/new', function(req, res){
+router.get('/new', function(req, res){
     res.render('campgrounds/new')
 });
 
-router.get('/campgrounds/:id', function(req, res){
+router.get('/:id', function(req, res){
     Campground.findById(req.params.id).populate('comments').exec(function(err, foundCampground){
         if(err){
             console.log(err);
