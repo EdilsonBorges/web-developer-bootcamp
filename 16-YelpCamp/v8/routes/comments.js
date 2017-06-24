c1b50d53bc0cd1cb50a25677128a3185422a4e67
Edilson Borges <edilsonfb@gmail.com>
2017-06-24 18:27:45 -0300

    Saving id for comment

--- 16-YelpCamp/v8/routes/comments.js
+++ 16-YelpCamp/v8/routes/comments.js
@@ -24,10 +24,13 @@ router.post('/', isLoggedIn, function(req, res){
                     console.log(err);
                 } else {
                     // add username and id to comment
-                    console.log('username is: '+req.user);
+                    comment.author.id = req.user._id;
+                    comment.author.username = req.user.username;
                     // save comment
+                    comment.save();
                     campground.comments.push(comment);
                     campground.save();
+                    console.log(comment);
                     res.redirect('/campgrounds/' + campground._id);
                 }
             });
