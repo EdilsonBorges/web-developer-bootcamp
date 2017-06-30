5a52a448590f8bec80d08912ef6de6b0a5239d51
Edilson Borges <edilsonfb@gmail.com>
2017-06-30 20:58:46 -0300

    checkCampgroundOwnershipt on index js

--- 16-YelpCamp/v10/middleware/index.js
+++ 16-YelpCamp/v10/middleware/index.js
@@ -1,3 +1,21 @@
 var middlewareObj = {};
 
+middlewareObj.checkCampgroundOwnershipt = function(req, res, next){
+    if(req.isAuthenticated()){
+        Campground.findById(req.params.id, function(err, foundCampground){
+            if(err){
+                res.redirect('back');
+            } else {
+                if(foundCampground.author.id.equals(req.user._id)){
+                    next();
+                } else {
+                    res.redirect('back');
+                }
+            }
+        });
+    } else {
+        res.redirect('back');
+    }
+};
+
 module.exporst = middlewareObj;
--- 16-YelpCamp/v10/routes/campgrounds.js
+++ 16-YelpCamp/v10/routes/campgrounds.js
@@ -88,22 +88,4 @@ function isLoggedIn(req, res, next){
     res.redirect('/login');
 };
 
-function checkCampgroundOwnership(req, res, next){
-    if(req.isAuthenticated()){
-        Campground.findById(req.params.id, function(err, foundCampground){
-            if(err){
-                res.redirect('back');
-            } else {
-                if(foundCampground.author.id.equals(req.user._id)){
-                    next();
-                } else {
-                    res.redirect('back');
-                }
-            }
-        });
-    } else {
-        res.redirect('back');
-    }
-}
-
 module.exports = router;
