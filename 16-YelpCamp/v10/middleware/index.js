9d3bcde816c99a156a9b09d613286aab6a7e3a53
Edilson Borges <edilsonfb@gmail.com>
2017-06-30 20:59:33 -0300

    checkCommentOwnership on index js

--- 16-YelpCamp/v10/middleware/index.js
+++ 16-YelpCamp/v10/middleware/index.js
@@ -18,4 +18,22 @@ middlewareObj.checkCampgroundOwnershipt = function(req, res, next){
     }
 };
 
+middlewareObj.checkCommentOwnership = function(req, res, next){
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+            if(err){
+                res.redirect('back');
+            } else {
+                if(foundComment.author.id.equals(req.user._id)){
+                    next();
+                } else {
+                    res.redirect('back');
+                }
+            }
+        });
+    } else {
+        res.redirect('back');
+    }
+}
+
 module.exporst = middlewareObj;
