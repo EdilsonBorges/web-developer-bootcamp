addfdcd02d1cf32c823e182272ff4e6d6b791506
Edilson Borges <edilsonfb@gmail.com>
2017-06-29 23:31:58 -0300

    checkCommentOwnership

--- 16-YelpCamp/v10/routes/comments.js
+++ 16-YelpCamp/v10/routes/comments.js
@@ -38,7 +38,7 @@ router.post('/', isLoggedIn, function(req, res){
     })
 });
 
-router.get('/:comment_id/edit', function(req, res){
+router.get('/:comment_id/edit', checkCommentOwnership, function(req, res){
     Comment.findById(req.params.comment_id, function(err, foundComment){
         if(err){
             res.redirect('back');
@@ -73,6 +73,24 @@ function isLoggedIn(req, res, next){
         return next();
     }
     res.redirect('/login');
+}
+
+function checkCommentOwnership(req, res, next){
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+            if(err){
+                res.redirect('back');
+            } else {
+                if(foundComment.author.id.equals(req.user._id)){
+                    next();
+                } else {
+                    res.redirect('back');
+                }
+            }
+        });
+    } else {
+        res.redirect('back');
+    }
 }
 
 module.exports = router;
