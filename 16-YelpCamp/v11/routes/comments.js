90693039f9534d56735d77d98f41e4f0655c4fb9
Edilson Borges <edilsonfb@gmail.com>
2017-07-01 17:04:45 -0300

    Flash messages for important actions

--- 16-YelpCamp/v11/routes/comments.js
+++ 16-YelpCamp/v11/routes/comments.js
@@ -22,6 +22,7 @@ router.post('/', middleware.isLoggedIn, function(req, res){
         } else{
             Comment.create(req.body.comment, function(err, comment){
                 if(err){
+                    req.flash('error','Something went wrong');
                     console.log(err);
                 } else {
                     // add username and id to comment
@@ -32,6 +33,7 @@ router.post('/', middleware.isLoggedIn, function(req, res){
                     campground.comments.push(comment);
                     campground.save();
                     console.log(comment);
+                    req.flash('success','Successfully added comment');
                     res.redirect('/campgrounds/' + campground._id);
                 }
             });
@@ -64,6 +66,7 @@ router.delete('/:comment_id', middleware.checkCommentOwnership, function(req, re
         if(err){
             res.redirect('back');
         } else {
+            req.flash('success','Comment deleted');
             res.redirect('/campgrounds/' + req.params.id);
         }
     });
