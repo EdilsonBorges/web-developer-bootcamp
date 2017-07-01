8d1eab7d22ff2b54d2c5e68a56a1bc141d011d61
Edilson Borges <edilsonfb@gmail.com>
2017-06-30 21:04:33 -0300

    Using middleware functions

--- 16-YelpCamp/v10/routes/campgrounds.js
+++ 16-YelpCamp/v10/routes/campgrounds.js
@@ -13,7 +13,7 @@ router.get('/', function(req, res){
     })
 });
 
-router.post('/', isLoggedIn, function(req, res){
+router.post('/', middleware.isLoggedIn, function(req, res){
     var name = req.body.name
     var image = req.body.image
     var desc = req.body.description
@@ -36,7 +36,7 @@ router.post('/', isLoggedIn, function(req, res){
     })
 });
 
-router.get('/new', isLoggedIn, function(req, res){
+router.get('/new', middleware.isLoggedIn, function(req, res){
     res.render('campgrounds/new')
 });
 
@@ -51,7 +51,7 @@ router.get('/:id', function(req, res){
 });
 
 //Edit Campground
-router.get('/:id/edit', checkCampgroundOwnership, function(req, res){
+router.get('/:id/edit', middleware.checkCampgroundOwnership, function(req, res){
     Campground.findById(req.params.id, function(err, foundCampground){
         if(err){
             res.redirect('/campgrounds');
@@ -61,7 +61,7 @@ router.get('/:id/edit', checkCampgroundOwnership, function(req, res){
     });
 });
 //Update Campground route
-router.put('/:id', checkCampgroundOwnership, function(req, res){
+router.put('/:id', middleware.checkCampgroundOwnership, function(req, res){
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
         if (err) {
             res.redirect('/campgrounds');
@@ -72,7 +72,7 @@ router.put('/:id', checkCampgroundOwnership, function(req, res){
 });
 
 //Destroy campground routea
-router.delete('/:id', checkCampgroundOwnership, function(req, res){
+router.delete('/:id', middleware.checkCampgroundOwnership, function(req, res){
     Campground.findByIdAndRemove(req.params.id, function(err){
         if(err){
             res.redirect('/campgrounds');
